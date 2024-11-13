b2270187efde049dd3919b2cb0975e4b625eddb3
khachikkar <khachikkarapetyan78@gmail.com>
2024-11-14 01:34:57 +0400

    must doed tasks on todo file

--- src/components/ProfileSection/index.js
+++ src/components/ProfileSection/index.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Input} from "antd";
+import {Input, Button, Upload} from "antd";
 import {ResumeContext} from "../../context";
+import { UploadOutlined } from '@ant-design/icons';
 import "./index.css"
 
 
@@ -12,7 +13,7 @@ const [name, setName] = useState("")
 const [lastname, setLastname] = useState("")
 const [phone, setPhone] = useState(0)
 const [address, setAddress] = useState("")
-
+const [imageUrl, setImageUrl] = useState(null);
 
     useEffect(() => {
         setCollectdata((prevData) => ({
@@ -20,6 +21,27 @@ const [address, setAddress] = useState("")
             profile: { name, lastname, phone, address },
         }));
     }, [name, lastname, phone, address, setCollectdata]);
+
+
+    const handleUploadChange = (info) => {
+        console.log(info)
+        // Only proceed if a file is uploaded
+        if (info.file.status === 'done') {
+            // Create a new FileReader instance
+            const reader = new FileReader();
+
+            // Read the uploaded file as a Data URL
+            reader.onload = () => {
+                // Set the result (data URL) as the image URL
+                setImageUrl(reader.result);
+                console.log('Image URL:', reader.result); // Logs the Data URL to console
+            };
+
+            // Start reading the file as a Data URL
+            reader.readAsDataURL(info.file.originFileObj);
+        }
+    };
+
 
 
     return (
@@ -49,6 +71,16 @@ const [address, setAddress] = useState("")
                 placeholder="Address"
                 style={{fontSize: "16px"}}
             />
+            <Upload
+                action="http://localhost:3001/main/builder"  // Specify the correct endpoint URL
+                showUploadList={false}
+                onChange={handleUploadChange}
+                onError={(error) => {
+                    console.error('Upload error:', error);
+                }} >
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            </Upload>
+            <img src={imageUrl} />
         </div>
     )
 }
